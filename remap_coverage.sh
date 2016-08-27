a5b659f82584430cfde5b89c7368a394ca31c2bc
Donovan Solms <donovan.solms@gmail.com>
2016-08-27 12:52:05 +0200

    Updated remapping

--- remap_coverage.sh
+++ remap_coverage.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# This script remaps the coverage paths to the actual Travis CI path
 sed -i 's/_\/home\/donovan\/Development\/project-limitless\/Limitless.Badger\/src\/badger/badger/g' coverage.out
-sed -i 's/_\/home\/travis\/gopath\/src\/github.com\/ProjectLimitless\/Badger\/src\//badger\/github.com\/ProjectLimitless\/Badger\/src\/badger/g' coverage.out
+sed -i 's/_\/home\/travis\/gopath\/src\/github.com\/ProjectLimitless\/Badger\/src/github.com\/ProjectLimitless\/Badger\/src\//g' coverage.out
